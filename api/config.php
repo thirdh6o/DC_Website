411b8678552065fa02a872cf26b479cd942e5338
Hing <128003687+thirdh6o@users.noreply.github.com>
2025-02-14 14:59:24 +0800

    Update config.php

--- api/config.php
+++ api/config.php
@@ -5,9 +5,9 @@ header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 
 $db_host = 'localhost';
-$db_user = 'root';
-$db_pass = 'b75dd9176641dc49';
-$db_name = 'message';
+$db_user = 'xxxxxx';
+$db_pass = 'xxxxxxx';
+$db_name = 'xxxxxx';
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8mb4", $db_user, $db_pass);
